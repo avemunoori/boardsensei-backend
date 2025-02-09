55a9ab16b55b9842557571735e4733edef8d0860
avemunoori <85594271+avemunoori@users.noreply.github.com>
2025-02-09 00:10:56 -0500

    updated schema

--- models/QuizModel.js
+++ models/QuizModel.js
@@ -1,15 +1,12 @@
 const mongoose = require("mongoose");
 
-const quizSchema = new mongoose.Schema(
-  {
-    openingName: { type: String}, 
-    questions: [
-      {
-        question: { type: String, required: true },
-        choices: [{ type: String, required: true }],
-        correctAnswer: { type: Number, required: true },
-      },
-    ],
-  },
-  { timestamps: true }
-);
+const quizSchema = new mongoose.Schema({
+  openingName: { type: String, default: "Unnamed Quiz" },
+  questions: [
+    {
+      question: { type: String, required: true },
+      options: [{ type: String, required: true }], 
+      answer: { type: String, required: true },     
+    },
+  ],
+}, { timestamps: true });
